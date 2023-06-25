c06f225afc16095143d264378428eec728de7191
Bryan Deckers <r0889617@student.thomasmore.be>
2023-06-25 12:10:44 +0200

    homepage more spacing

--- code/dfkwebsite/pages/index.tsx
+++ code/dfkwebsite/pages/index.tsx
@@ -9,18 +9,21 @@ const HomePage: NextPage = () => {
         <title>DFK | Startpagina</title>
         <meta name="description" content="DFK startpagina" />
       </Head>
-      <ImageRead
-        title="This is a title"
-        summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
-        date={1}
-      />
-      <ImageRead
-        title="This is a title"
-        summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,  quis nostrud exercitation ullamco laboris..."
-        order={true}
-        date={1}
-      />
-      <div>
+      <div className="flex flex-col gap-24">
+        <ImageRead
+          title="This is a title"
+          summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+          date={1}
+        />
+        <ImageRead
+          title="This is a title"
+          summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,  quis nostrud exercitation ullamco laboris..."
+          order={true}
+          date={1}
+        />
+      </div>
+
+      <div className="mt-20">
         <h1 className="font-extrabold text-3xl sm:text-5xl mb-1 sm:mb-5 text-black dark:text-white">
           Toernooi in de kijker
         </h1>
