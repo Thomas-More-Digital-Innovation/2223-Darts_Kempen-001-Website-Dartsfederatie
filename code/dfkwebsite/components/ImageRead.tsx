aa0b5758706d50ca07a99f5a0f6fb46446faa088
Bryan Deckers <r0889617@student.thomasmore.be>
2023-06-26 12:42:22 +0200

    posts smaller on homepage

--- code/dfkwebsite/components/ImageRead.tsx
+++ code/dfkwebsite/components/ImageRead.tsx
@@ -13,14 +13,14 @@ type imageData = {
 
 const ImageRead: FunctionComponent<imageData> = (imageData: imageData) => {
   return (
-    <div className="grid w-full grid-cols-1 lg:grid-cols-2 gap-5 lg:gap-20 mb-10">
+    <div className="grid w-full grid-cols-1 lg:grid-cols-2 gap-5 lg:gap-10 mb-10">
       <div className={`${imageData.order ? "lg:order-1" : "lg:order-0"}`}>
         <Image
           src={dfkLogo}
           alt="test"
           className={`bg-light-gray object-cover max-w-full relative overflow-hidden aspect-video `}
           height={400}
-          width={700}
+          width={600}
         />
       </div>
       <div
@@ -28,7 +28,7 @@ const ImageRead: FunctionComponent<imageData> = (imageData: imageData) => {
           imageData.order ? "lg:order-0" : "lg:order-1 lg:text-left"
         }`}
       >
-        <h1 className=" font-extrabold text-4xl sm:text-5xl mb-1">
+        <h1 className="font-extrabold text-2xl sm:text-3xl mb-1">
           {imageData.title}
         </h1>
         <h6 className="text-white text-sm mb-6">
@@ -38,10 +38,10 @@ const ImageRead: FunctionComponent<imageData> = (imageData: imageData) => {
             year: "numeric",
           })}
         </h6>
-        <p className="text-xl lg:text-2xl ">{imageData.summary}</p>
+        <p className="text-lg lg:text-xl">{imageData.summary}</p>
 
         <Link
-          className="text-white dark:text-blacktext w-fit text-xl lg:text-2xl py-4 lg:py-6 px-6 lg:px-10 mt-4 lg:mt-12 bg-neutral-700 hover:bg-neutral-800 dark:bg-gray-200 dark:hover:bg-gray-300"
+          className="text-white dark:text-blacktext w-fit text-lg lg:text-xl py-4 lg:py-4 px-4 lg:px-10 mt-4 lg:mt-12 bg-neutral-700 hover:bg-neutral-800 dark:bg-gray-200 dark:hover:bg-gray-300"
           href={`/info/nieuws/${imageData.title
             .toLowerCase()
             .replace(" ", "-")}`}
