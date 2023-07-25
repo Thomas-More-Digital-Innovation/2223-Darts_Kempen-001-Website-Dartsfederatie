c9351fa05b8d22f6b0263ce50e89e9a40c9456c5
Syan Delbart <syan.delbart@hotmail.com>
2023-07-25 14:20:16 +0200

    Modified data type of posts

--- code/dfkwebsite/types/posts.ts
+++ code/dfkwebsite/types/posts.ts
@@ -1,15 +1,18 @@
+import { User } from "./user";
+
 export type Post = {
   postID: string;
   title: string;
-  datePublished: number;
+  dateCreated: number;
+  datePublished?: number;
   text: string;
-  authorID?: string;
-  dateCreated?: number;
-  src: string;
-  srcAlt: string;
+  authorID: string;
+  thumbnail: string;
+  thumbnailAlt: string;
   deleted?: boolean;
 };
 
 export interface PostFront extends Post {
   textFormatted: string; // Text will be stored in markdown format in the database, it needs to be converted to HTML for display
+  author: User;
 }
