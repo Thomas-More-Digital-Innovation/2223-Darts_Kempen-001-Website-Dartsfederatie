7a2ac58cb3294e0758ae1ea6204c12250c035637
Syan Delbart <syan.delbart@hotmail.com>
2023-07-25 14:17:13 +0200

    Renamed news to posts in types

--- code/dfkwebsite/types/posts.ts
+++ code/dfkwebsite/types/posts.ts
@@ -1,6 +1,15 @@
 export type Post = {
-    
+  postID: string;
+  title: string;
+  datePublished: number;
+  text: string;
+  authorID?: string;
+  dateCreated?: number;
+  src: string;
+  srcAlt: string;
+  deleted?: boolean;
 };
 
-export interface PostsFront extends Post {
+export interface PostFront extends Post {
+  textFormatted: string; // Text will be stored in markdown format in the database, it needs to be converted to HTML for display
 }
